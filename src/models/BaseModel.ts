f5c16dac5304db57676ea3bbbcebe68e54f656df
Mark Kevin Besinga <markkevin.besinga@hcl.com>
2025-04-14 02:28:59 +0800

    fixed red lines

--- src/models/BaseModel.ts
+++ src/models/BaseModel.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge'
 
 let prisma: PrismaClient;
 
@@ -79,6 +79,6 @@ export abstract class BaseModel<T, CreateInput, UpdateInput> {
 
   // Utility methods
   protected getPrismaModel() {
-    return this.prisma[this.modelName];
+    return this.prisma[this.modelName] as any;
   }
 } 
